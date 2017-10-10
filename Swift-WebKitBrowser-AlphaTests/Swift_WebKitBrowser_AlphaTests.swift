81e3f77e67c7994b4f2f9e0b945d713ccf28c65d
takeuchiken <takeuchiken@as-macbook-pro.local>
2017-10-11 01:52:45 +0900

    Initial Commit

---
+++ Swift-WebKitBrowser-AlphaTests/Swift_WebKitBrowser_AlphaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Swift_WebKitBrowser_AlphaTests.swift
+//  Swift-WebKitBrowser-AlphaTests
+//
+//  Created by 竹内健 on 2017/10/11.
+//  Copyright © 2017年 竹内健. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_WebKitBrowser_Alpha
+
+class Swift_WebKitBrowser_AlphaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
