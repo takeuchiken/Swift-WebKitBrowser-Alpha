803d1d2d54f7d7ea6c69ede311d90a64a81794e5
takeuchiken <takeuchiken@as-macbook-pro.local>
2017-10-11 03:01:02 +0900

    2nd commit

--- Swift-WebKitBrowser-Alpha/ViewController.swift
+++ Swift-WebKitBrowser-Alpha/ViewController.swift
@@ -9,16 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	var wk = WebViewController()
+	
+	override func loadView() {
+		super.loadView()
+		
+		print("ViewController.loadView Finished")
+	}
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
+		wk.loadWebView()
+		// webViewが入るframeをviewのframeに指定
+		wk.webView.frame = view.frame
+		// 親ViewにWKWebViewを追加
+		view.addSubview(wk.webView)
+		// WKWebViewを最背面に移動
+		view.sendSubview(toBack: wk.webView)
+		
+		wk.loadWebContent()
+		
+		print("ViewController.viewDidLoad Finished")
 	}
 
+	override func viewDidDisappear(_ animated: Bool) {
+		
+		
+		print("ViewController.viewDidDisappear Finished")
+	}
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	
 
 
 }
